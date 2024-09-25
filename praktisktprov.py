1c4d63020f3d1b5483db5a1a0eb842de3d76c470
Mister Magoo <d1liadvo@skola.goteborg.se>
2024-09-25 09:16:38 +0200

    Commitar praktisktprov

---
+++ praktisktprov.py
@@ -0,0 +1,16 @@
+#Author: Liam Dvoretsky
+#Date: 2024-09-92
+
+product = 1
+for num in input("Ange siffror separerade med mellanslag: ").split():
+        if num.isdigit():
+            num = int(num)
+            print(f"\nMultiplikationstabell för {num}:")
+            for i in range(1, 11):
+                print(f"{num} * {i} = {num * i}")
+        else:
+            print("Fel: Ange endast siffror separerade med mellanslag.a")
+print("\nProgram avslutat.")
+
+
+values = input ("Vilka tabeller vill du beräkna?: ")
